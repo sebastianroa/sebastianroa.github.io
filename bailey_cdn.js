26fe77825cad9303f6f0e68b3b6e5fbe2e81818f
sebastianroa <sebastian.roa@live.com>
2021-11-29 15:20:30 -0500

    Update bailey_cdn.js

--- bailey_cdn.js
+++ bailey_cdn.js
@@ -1,8 +1,10 @@
+function createAlert() {
+	alert("testing");
+}
+
 const fab = document.getElementById("floating-right-bottom-btn");
 fab.style.position = 'absolute';
 fab.style.right = '10px';
 fab.style.bottom = '16px';
 
-function createAlert() {
-	alert("testing");
-}
+
