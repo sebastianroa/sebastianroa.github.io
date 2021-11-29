299ccbc14deaaf28014b06b3a0ff21d61ae2549c
sebastianroa <sebastian.roa@live.com>
2021-11-29 15:17:29 -0500

    Update bailey_cdn.js

--- bailey_cdn.js
+++ bailey_cdn.js
@@ -2,9 +2,7 @@ const fab = document.getElementById("floating-right-bottom-btn");
 fab.style.position = 'absolute';
 fab.style.right = '10px';
 fab.style.bottom = '16px';
-fab.style.backgroundColor = '#6FBC6D';
-fab.style.height = '100px';
-fab.style.width = '100px;
+fab.style.backgroundColor = 'red';
 
 function createAlert() {
 	alert("testing");
